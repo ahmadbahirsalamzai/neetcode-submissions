0b2e14a5aae9b16c585b5f14317c6f5b95188e46
Ahmad Bahir Salamzai <141477204+ahmadbahirsalamzai@users.noreply.github.com>
2026-07-23 11:19:40 -0700

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    # time = O(n)
+    # Space = O(1)
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return
+
+        prev1 = 0
+        prev2 = 0
+
+        # itterate through the list of nums
+        for i in range(len(nums)):
+            temp = prev1
+            prev1 = prev2 + nums[i] if prev2 + nums[i] > prev1 else prev1
+
+            prev2 = temp
+
+        # return the max of prev1, prev2
+        return prev1
