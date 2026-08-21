cdacb40a04b0e62003c638ae3b8503080fc8de3d
Ahmad Bahir Salamzai <141477204+ahmadbahirsalamzai@users.noreply.github.com>
2026-08-21 13:12:05 -0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack = self.stack[:-1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        minCurr = self.stack[0]
+
+        for curr in self.stack:
+            if curr < minCurr:
+                minCurr = curr
+        return minCurr
+
+        
