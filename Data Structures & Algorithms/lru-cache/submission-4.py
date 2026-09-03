ee25a99ba863660ea0e8fc1f62aa7b4a63796932
Ahmad Bahir Salamzai <141477204+ahmadbahirsalamzai@users.noreply.github.com>
2026-09-03 09:47:21 -0700

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,24 @@
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()  # [[key,value]]
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        # if the key is already there move to the end
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        # if the key is in the dict then update the value and move ot the end and return
+        if key in self.cache:
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+            return
+        
+        # if cache is full then pop the first element and then add move the new key to the end
+        if len(self.cache) == self.capacity:
+            self.cache.pop(next(iter(self.cache)))
+
+        self.cache[key] = value
