c3ad7bdd402534a5c2ba418821aa49147362326e
Ahmad Bahir Salamzai <141477204+ahmadbahirsalamzai@users.noreply.github.com>
2026-09-16 13:24:17 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    # approch:
+    # first calculate all the distances for all the points while keeping track if them in a list 'heap' and then heapify that list and then do queuepop k times
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # Time worst case time: O(nlogn)
+        # space: O(n) if we don't count the reslt list
+        heap = [] # heap[i] = [res, x, y]
+
+        for x,y in points:
+            temp = math.sqrt(x**2 + y**2)
+            heap.append([temp, x, y])
+
+        result = []
+        heapq.heapify(heap)
+
+        for _ in range(k):
+            res, x, y = heapq.heappop(heap)
+            result.append([x,y])
+
+        return result
