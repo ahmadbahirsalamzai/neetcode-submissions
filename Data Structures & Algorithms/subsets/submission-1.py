1f1d0d5d35c5db322e47f18480ea3bae00ab84f4
Ahmad Bahir Salamzai <141477204+ahmadbahirsalamzai@users.noreply.github.com>
2026-09-25 10:08:58 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # time: O(n * 2^n)
+        # space: O(n) and O(2^n) for the out put list. 
+        res = []
+        curr = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(curr[:])
+                return
+            curr.append(nums[i])
+            dfs(i + 1)
+            curr.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
